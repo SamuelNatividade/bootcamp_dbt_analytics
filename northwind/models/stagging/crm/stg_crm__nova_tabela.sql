95d275d05c7196b4d75db7fd36e8b09204d1c297
samuel_natividade <samuell.natividade@gmail.com>
2024-06-04 22:23:36 -0300

    dbt models e dbt seed

---
+++ northwind/models/stagging/crm/stg_crm__nova_tabela.sql
@@ -0,0 +1,9 @@
+WITH renamed AS     (
+                    SELECT
+                        category_id as id
+                        ,category_name as "name"
+                        ,description
+                        ,updated_at
+                    from {{ref('raw_crm__nova_tabela')}}
+                    )
+select * from renamed
